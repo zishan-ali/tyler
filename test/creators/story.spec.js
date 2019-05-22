c5f5034c53de991d5269f1531cc3ad3a59b37007
Zishan Ali <zishan.ali@sky.uk>
2019-05-22 14:21:09 +0100

    Fix Story test

--- test/creators/story.spec.js
+++ test/creators/story.spec.js
@@ -1,10 +1,10 @@
-import mockfs from 'mock-fs';
-import fs from 'fs';
-import { createStory } from '../../src/creators';
-import { storyFixture } from '../../src/fixtures';
+import mockfs from "mock-fs";
+import fs from "fs";
+import { createStory } from "../../src/creators";
+import { storyFixture, storyIndexFixture } from "../../src/fixtures";
 
-describe('.createStory/2', async () => {
-  const parentPath = './src/foo-bar';
+describe(".createStory/2", async () => {
+  const parentPath = "./src/foo-bar";
 
   beforeEach(() => {
     mockfs({
@@ -16,13 +16,15 @@ describe('.createStory/2', async () => {
     mockfs.restore();
   });
 
-  it('creates an `story` file in the given directory', (done) => {
-    createStory(parentPath, {})
-      .then(() => {
-        expect(
-          fs.readFileSync(`${parentPath}/story.js`, { encoding: 'utf8' })
-        ).toEqual(storyFixture);
-        done();
-      });
+  it("creates an `index` and `Default` file in the given directory", done => {
+    createStory(parentPath, {}).then(() => {
+      expect(
+        fs.readFileSync(`${parentPath}/story/index.js`, { encoding: "utf8" })
+      ).toEqual(storyIndexFixture);
+      expect(
+        fs.readFileSync(`${parentPath}/story/Default.jsx`, { encoding: "utf8" })
+      ).toEqual(storyFixture);
+      done();
+    });
   });
 });
